f310d2480299e5e165a8d26348cd2ceb08085fe9
David Bloss <70458676+davidbloss@users.noreply.github.com>
2022-09-01 15:39:38 -0500

    rm comment plugin, add vim-bbye

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -19,10 +19,10 @@ return packer.startup(function(use)
   use { "wbthomason/packer.nvim" } -- Have packer manage itself
   use { "nvim-lua/plenary.nvim" } -- Useful lua functions used by lots of plugins
   use { "windwp/nvim-autopairs" } -- Autopairs, integrates with both cmp and treesitter
-  use { "numToStr/Comment.nvim" }
   use { "kyazdani42/nvim-tree.lua" }
   use { "kyazdani42/nvim-web-devicons" }
   use { "akinsho/bufferline.nvim" }
+  use { "moll/vim-bbye" }
   use { "mbbill/undotree" }
   use { "nvim-lualine/lualine.nvim" }
   use { "akinsho/toggleterm.nvim" }
